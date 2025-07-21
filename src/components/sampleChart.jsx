02b3deb35c0cc1d5a832e5e27168496a6ab6a8e8
Alfredo <193377562+A-Galicia@users.noreply.github.com>
2025-07-21 15:20:57 -0700

    added sample chart

---
+++ src/components/sampleChart.jsx
@@ -0,0 +1,51 @@
+import classes from '../styles/sample.module.css';
+
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  Label,
+  ResponsiveContainer,
+  AreaChart,
+} from 'recharts';
+
+const data = (() => {
+  let data = [];
+  for (let i = 0; i < 30; i++) {
+    let x = (3 * i) ** (1 / 3) - i + (0.2 * i) ** 2 + 225;
+    const fixedX = x.toFixed(2);
+    data.push({ strength: fixedX });
+  }
+  return data;
+})();
+
+function Sample() {
+  return (
+    <div className={classes.main}>
+      <ResponsiveContainer width='100%' height='100%'>
+        <AreaChart data={data} margin={{ bottom: 50, left: 50 }}>
+          <Line
+            type='monotone'
+            dataKey='strength'
+            stroke='#172c3dff'
+            strokeWidth={3}
+            dot={false}
+          ></Line>
+          <XAxis stroke='#000000ff'>
+            <Label className={classes.label} value='days' position='bottom' />
+          </XAxis>
+          <YAxis domain={['auto', 'auto']} stroke='#000000ff'>
+            <Label className={classes.label} value='lbs' position='left' />
+          </YAxis>
+          <Tooltip />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
+
+export default Sample;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,18 @@
 import classes from '../styles/home.module.css';
+import Sample from '../components/sampleChart';
 
 function Home() {
   return (
     <div className={classes.main}>
       <h1>Strength Projector</h1>
-
+      <Sample></Sample>
       <nav>
         <ul>
           <li>
             <a>Tryout the App</a>
           </li>
           <li>
-            <a>Create and account</a>
+            <a>Create an account</a>
           </li>
           <li>
             <a
